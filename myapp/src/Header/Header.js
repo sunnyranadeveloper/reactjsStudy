5e0f0cea9d642bd3e3c9bd723487f8170c595ef9
Sunny Rana -X (sunnrana) <sunnrana@cisco.com>
2019-09-18 01:11:45 +0530

    concept of two way binding

--- myapp/src/Header/Header.js
+++ myapp/src/Header/Header.js
@@ -9,6 +9,11 @@ const header = (props) => {
             projectName : [{name: "Inner 1 Clicked"},{name: "Inner 2 Clicked"}]
         });
     }
+    const changeValue = (event) => {
+        setHeaderState({
+            projectName : [{name: event.target.value},{name: "Inner 2 Clicked"}]
+        });
+    }
     //it use to display function based compononent
     return (
         <div>
@@ -19,6 +24,12 @@ const header = (props) => {
             <button onClick={switchClickHandler}>Click Here</button>
             {/* replica of out button */}
             <button onClick={props.click}>Outer copy</button>
+            {/* Example of two way binding !!! if you use () then it will call function on load*/}
+            {/* Two way binding means it 
+                1. read data from DOM like reading from p tag
+                2. It write data in DOM like on change it is writing data in p tag. 
+            */}
+            <input type="text" name="test" onChange={changeValue} value={headerState.projectName[0].name} />
         </div>
     );
 }
